d393b93a62d3bc777ef1969539a4badc314625ad
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-07-15 09:32:45 +0200

    add files from the movie_api directory

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Models = require('./models.js');
 const bodyParser = require('body-parser');
 const Movies = Models.Movie;
 const Users = Models.User;
+const validator = require('express-validator');
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
@@ -13,6 +14,8 @@ const express = require('express');
 const app = express();
 //importing morgan
 const morgan = require('morgan');
+//use express validator library
+app.use(validator());
 //serves documentation.html file from public folder
 app.use(express.static('public'));
 //logs requests using Morgan’s “common” format
@@ -23,7 +26,23 @@ var auth = require('./auth')(app);
 //require the Passport module and import the “passport.js” file
 const passport = require('passport');
 require('./passport');
-//declaring variable for movie list
+//importing and using CORS
+const cors = require('cors');
+app.use(cors());
+
+//CORS code to allow requests from only certain origins to be given access
+var allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // GET requests
 app.get('/', function(req, res) {
@@ -79,24 +98,40 @@ app.get('/directors/:Name', passport.authenticate('jwt', { session: false }), fu
 });
 
 //Add a user
-app.post('/users', function(req, res) {
-  Users.findOne({ Username : req.body.Username })
+app.post('/users', (req, res) => {
+  // Validation logic here for request
+  req.checkBody('Username', 'Username is required').notEmpty();
+  req.checkBody('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric()
+  req.checkBody('Password', 'Password is required').notEmpty();
+  req.checkBody('Email', 'Email is required').notEmpty();
+  req.checkBody('Email', 'Email does not appear to be valid').isEmail();
+
+  // check the validation object for errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    return res.status(422).json({ errors: errors });
+  }
+
+  var hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username : req.body.Username }) // Search to see if a user with the requested username already exists
   .then(function(user) {
     if (user) {
-      return res.status(400).send(req.body.Username + "already exists");
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + " already exists");
     } else {
       Users
       .create({
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
+        Username : req.body.Username,
+        Password: hashedPassword,
+        Email : req.body.Email,
+        Birthday : req.body.Birthday
       })
-      .then(function(user) {res.status(201).json(user) })
+      .then(function(user) { res.status(201).json(user) })
       .catch(function(error) {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      })
+          console.error(error);
+          res.status(500).send("Error: " + error);
+      });
     }
   }).catch(function(error) {
     console.error(error);
@@ -130,6 +165,20 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), fu
 
 // Update a user's info, by username
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), function(req, res) {
+  // Validation logic here for request
+  req.checkBody('Username', 'Username is required').notEmpty();
+  req.checkBody('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric()
+  req.checkBody('Password', 'Password is required').notEmpty();
+  req.checkBody('Email', 'Email is required').notEmpty();
+  req.checkBody('Email', 'Email does not appear to be valid').isEmail();
+
+  // check the validation object for errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    return res.status(422).json({ errors: errors });
+  }
+
   Users.findOneAndUpdate({ Username : req.params.Username }, { $set :
   {
     Username : req.body.Username,
@@ -205,4 +254,7 @@ app.use(function (err, req, res, next) {
 });
 
 // listen for requests
-app.listen(8080);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
--- models.js
+++ models.js
@@ -1,4 +1,7 @@
+//importing mongoose
 const mongoose = require('mongoose');
+//importing bcrypt
+const bcrypt = require('bcrypt');
 
 var movieSchema = mongoose.Schema({
   Title : {type: String, required: true},
@@ -17,12 +20,20 @@ var movieSchema = mongoose.Schema({
 });
 
 var userSchema = mongoose.Schema({
- Username : {type: String, required: true},
- Password : {type: String, required: true},
- Email : {type: String, required: true},
- Birthday : Date,
- FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  Username : {type: String, required: true},
+  Password : {type: String, required: true},
+  Email : {type: String, required: true},
+  Birthday : Date,
+  FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
 });
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password); };
+};
 
 var Movie = mongoose.model('Movie', movieSchema);
 var User = mongoose.model('User', userSchema);
